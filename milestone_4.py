5ce1334780f256df2eb1e24582b4bd2bdab7b117
Ara <ara_404@outlook.com>
2023-08-23 12:18:34 +0100

    Milestone 4 changes made and completed

--- milestone_4.py
+++ milestone_4.py
@@ -9,25 +9,24 @@ class Hangman():
   self.word_list = word_list
   self.num_lives = num_lives
   self.word = random.choice(word_list)
-  self.word_guessed = ['_' for _ in self.word]
+  self.word_guessed = ['_'] * len(self.word)
   self.num_letters = len(set(self.word))
   self.list_of_guesses = []
 
  
- def check_guess(self, guess):
-  guess = guess.lower()
-  
-  if guess in self.word:
-   print(f"Good guess! {guess} is in the word.")
-   for i, letter in enumerate(self.word):
-    if letter == guess:
-     self.word_guessed[i] = guess
-    self.num_letters -= 1
-    print("Word guessed so far:", ' '.join(self.word_guessed))
-  else:
-     num_lives -= 1
-     print(f"Sorry, {letter} is not in the word. Try again.")
-     print(f"You have {num_lives} lives left.")
+ 
+  def check_guess(self, guess):
+   guess = guess.lower()
+   if guess in self.word:
+    print(f"Good guess! {guess} is in the word.")
+    for i in range(len(self.word)):
+     if self.word[i] == guess:
+      self.word_guessed[i] = guess
+      self.num_letters -= 1
+   else:
+    self.num_lives -= 1
+    print(f"Sorry, {guess} is not in the word.")
+    print(f"You have {self.num_lives} lives left.")
 
 
  def ask_for_input(self):
@@ -40,4 +39,4 @@ class Hangman():
    else:
     self.check_guess(guess)
     self.list_of_guesses.append(guess)
-
+    break
--- milestone_5.py
+++ milestone_5.py
@@ -9,27 +9,24 @@ class Hangman():
   self.word_list = word_list
   self.num_lives = num_lives
   self.word = random.choice(word_list)
-  self.word_guessed = ['_' for _ in self.word]
+  self.word_guessed = ['_'] * len(self.word)
   self.num_letters = len(set(self.word))
   self.list_of_guesses = []
 
  
+ 
  def check_guess(self, guess):
-  guess = guess.lower()
-  
-  if guess in self.word:
-   print(f"Good guess! {guess} is in the word.")
-   for i, letter in enumerate(self.word):
-    if letter == guess:
-     self.word_guessed[i] = guess
-    self.num_letters -= 1
-    print("Word guessed so far:", ' '.join(self.word_guessed))
-  else:
-   print(f"Sorry, {guess} is not in the word. Try again.")
-  for i, letter in enumerate(self.word):
-    if letter == guess:
-     self.word_guessed[i] = guess
-  self.num_letters -= 1
+   guess = guess.lower()
+   if guess in self.word:
+    print(f"Good guess! {guess} is in the word.")
+    for i in range(len(self.word)):
+     if self.word[i] == guess:
+      self.word_guessed[i] = guess
+      self.num_letters -= 1
+   else:
+    self.num_lives -= 1
+    print(f"Sorry, {guess} is not in the word.")
+    print(f"You have {self.num_lives} lives left.")
 
 
  def ask_for_input(self):
@@ -42,20 +39,20 @@ class Hangman():
    else:
     self.check_guess(guess)
     self.list_of_guesses.append(guess)
+    break
     
 def play_game(word_list):
- num_lives = 5
- game = Hangman(word_list=word_list, num_lives=num_lives)
+  num_lives = 5
+  game = Hangman(word_list, num_lives)
 
- while True:
-  if game.num_lives == 0:
-   print("You lost!")
-   break
-  elif game.num_letters > 0:
-   game.ask_for_input()
-   
-  else:
-   print("Congratulations. You won the game!")
-   break
+  while True:
+   if game.num_lives == 0:
+    print("You lost!")
+    break
+   elif game.num_letters > 0:
+    game.ask_for_input()
+   else:
+    print("Congratulations. You won the game!")
+    break
 
 play_game(word_list)
